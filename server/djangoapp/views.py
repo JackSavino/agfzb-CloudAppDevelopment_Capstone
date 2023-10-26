1111ad98245229d719436e1c4d5b4349ceac6a94
jacksavino <jack_savino@yahoo.com>
2023-10-26 12:51:21 -0400

    Adding changes

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -75,7 +75,7 @@ def registration_request(request):
 # Dealerships view to render the index page with a list of dealerships
 def get_dealerships(request):
     if request.method == "GET":
-        url = "your-cloud-function-domain/dealerships/dealer-get"
+        url = "djangoapp/dealership-package/get_dealerships"
         # Get dealers from the URL
         dealerships = get_dealers_from_cf(url)
         # Concat all dealer's short name
